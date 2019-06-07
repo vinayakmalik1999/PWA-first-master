e580ad695896112e12f30458e3eccae8da3a23f7
vmalik <vinayakmalik1999@gmail.com>
2019-06-07 12:58:19 -0500

    minor changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,4 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
+
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
@@ -27,7 +11,7 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
+  //Precache static resources here.
 
   self.skipWaiting();
 });
