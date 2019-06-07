6e96f8650603397b10eac4632f48259a06e498e1
vmalik <vinayakmalik1999@gmail.com>
2019-06-07 16:13:43 -0500

    test atom

--- public/service-worker.js
+++ public/service-worker.js
@@ -29,6 +29,7 @@ self.addEventListener('activate', (evt) => {
           if (key !== CACHE_NAME) {
             console.log('[ServiceWorker] Removing old cache', key);
             return caches.delete(key);
+            
           }
         }));
       })
