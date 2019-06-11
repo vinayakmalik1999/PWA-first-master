eb0262ca3c6fb0f97e03fa57dddcc59fa6bacd17
vmalik <vinayakmalik1999@gmail.com>
2019-06-11 12:16:57 -0500

    online changes 1

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,45 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v3';
+const DATA_CACHE_NAME = 'data-cache-v2';
 
-// CODELAB: Add list of files to cache here.
+// CODELAB: Add list of fi  les to cache here.
 const FILES_TO_CACHE = [
-  './offline.html',
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/scripts/install.js',
+  '/scripts/luxon-1.11.4.js',
+  '/styles/inline.css',
+  '/images/add.svg',
+  '/images/clear-day.svg',
+  '/images/clear-night.svg',
+  '/images/cloudy.svg',
+  '/images/fog.svg',
+  '/images/hail.svg',
+  '/images/install.svg',
+  '/images/partly-cloudy-day.svg',
+  '/images/partly-cloudy-night.svg',
+  '/images/rain.svg',
+  '/images/refresh.svg',
+  '/images/sleet.svg',
+  '/images/snow.svg',
+  '/images/thunderstorm.svg',
+  '/images/tornado.svg',
+  '/images/wind.svg',
+  '/manifest.json',
+  
 ];
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   //Precache static resources here.
   evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
@@ -24,15 +48,15 @@ self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
   // CODELAB: Remove previous cached data from disk.
   evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-            
-          }
-        }));
-      })
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+
+        }
+      }));
+    })
   );
   self.clients.claim();
 });
@@ -40,18 +64,61 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
   //  Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-  // if not a page navigation, get out.
-  return;
-}
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-          return caches.open(CACHE_NAME)
-              .then((cache) => {
-                return cache.match('offline.html');
+  if (evt.request.url.includes('/forecast/')) {
+    console.log('[Service Worker] Fetch (data)', evt.request.url);
+    evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+          return fetch(evt.request)
+              .then((response) => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+                return response;
+              }).catch((err) => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
               });
-        })
-);
+        }));
+    return;
+  }
+  evt.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(evt.request)
+            .then((response) => {
+              return response || fetch(evt.request);
+            });
+      })
+  );
+/*  self.addEventListener('fetch', function(event) {
+  console.log('Fetch' ,event);
+
+  event.respondWith(
+    caches.match(event.request)
+    .catch(function() {
+    return fetch(event.request);
+  }).then(function(response) {
+
+    caches.open(CACHE_NAME).then(function(cache) {
+      cache.put(event.request, response.clone());
+    });
+    return response;
+  }).catch(function(err) {
+    console.log('there was an error fetching');
+  }));
+});
+  if (evt.request.mode !== 'navigate') {
+    // if not a page navigation, get out.
+    return;
+  } */
+  /*evt.respondWith(
+    fetch(evt.request)
+    .catch(() => {
+      return caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.match('index.html');
+      });
+    }
+  ));*/
 
 });
